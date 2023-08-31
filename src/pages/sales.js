9e6c5e5d9e65f30048046797a182eff140a2baa3
unknown <you@example.com>
2023-08-31 21:43:20 +0800

    changes

--- src/pages/sales.js
+++ src/pages/sales.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Layout as DashboardLayout } from "src/layouts/dashboard/layout";
-import { Box, Container, Typography } from "@mui/material";
+import { Box, Container, Typography, Stack, Button , Unstable_Grid2 as Grid} from "@mui/material";
+import PlusIcon from "@heroicons/react/24/solid/PlusIcon";
+import { SvgIcon } from "@mui/material";
 import Head from "next/head";
 
 const Page = () => {
@@ -11,7 +13,22 @@ const Page = () => {
       </Head>
       <Box sx={{ py: 8, flexGrow: 1 }}>
         <Container maxWidth="xl" component="main">
-          <Typography variant="h4">Sales Stats</Typography>
+          <Stack justifyContent="space-between" direction="row">
+            <Typography variant="h4">Sales Statistics</Typography>
+            <Button
+              startIcon={
+                <SvgIcon>
+                  <PlusIcon />
+                </SvgIcon>
+              }
+              variant="contained"
+            >
+              Add Sales
+            </Button>
+          </Stack>
+          <Grid marginTop={3} container>
+                <Typography>rex</Typography>
+          </Grid>
         </Container>
       </Box>
     </>
