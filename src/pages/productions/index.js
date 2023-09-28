7846fa9802087b9725a9477580bf37b9ace35dc0
unknown <rockph0029@gmail.com>
2023-09-28 16:48:47 +0800

    changes

--- src/pages/productions/index.js
+++ src/pages/productions/index.js
@@ -1,8 +1,22 @@
+import { Box, Button, Container, Stack, Typography } from "@mui/material";
+import Head from "next/head";
 import React from "react";
 import { Layout as DashboardLayout } from "src/layouts/dashboard/layout";
 
 const Page = () => {
-  return <div>Page</div>;
+  return <>
+  <Head>
+    <title>Production ||</title>
+  </Head>
+  <Box sx={{flexGrow: 1, py: 8}}>
+    <Container component="main" maxWidth="xl">
+     <Stack direction="row" justifyContent="space-between">
+      <Typography variant="h4">Production Stats</Typography>
+      <Button variant="contained">Add proudctions</Button>
+     </Stack>
+    </Container>
+  </Box>
+  </>
 };
 
 Page.getLayout = (props) => <DashboardLayout>{props}</DashboardLayout>;
